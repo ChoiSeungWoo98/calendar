cdeaeeba3c7a72b2f58c9c71f53fb02d4ab54380
ChoiseU <csw980928@gmail.com>
2023-12-31 15:15:07 +0900

    refactor : 날씨 정보 현 위치로 받도록 수정

--- src/main/resources/static/js/weather.js
+++ src/main/resources/static/js/weather.js
@@ -1,29 +1,42 @@
-// 지역 추가 시 https://openweathermap.org/city/1897000 접속 후 검색
 const API_KEY="17be97eede6573f3de9cc7b23db3d280";
-const SI_HEUNG = 'Siheung-si';
-const SEONG_NAM = 'Seongnam-si';
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + SI_HEUNG + ',kr&appid=' + API_KEY + '&units=metric'
 
-$.getJSON(url,
-    function (WeatherResult) {
-        setAreaName();
-        $('.temperatures').append(WeatherResult.main.temp + ' °C');
-
-        var weathericonUrl =
-            '<img src= "http://openweathermap.org/img/wn/'
-            + WeatherResult.weather[0].icon +
-            '@2x.png" alt="' + WeatherResult.weather[0].description + '"/>'
-
-        $('.weather-ico').html(weathericonUrl);
-    });
-
-function setAreaName() {
-    if(url.includes(SEONG_NAM)) {
-        $('.position').html('경기도 성남시')
-        return;
-    }
-    if(url.includes(SI_HEUNG)) {
-        $('.position').html('경기도 시흥시')
-        return;
+function getLocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+            getWeather(lat, lon)
+        });
     }
 }
+
+function getWeather(lat, lon) {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + API_KEY;
+
+    $.getJSON(url, function (weatherResult) {
+        let temp = (weatherResult.main.temp - 273.15).toFixed(2);
+        setAreaName(weatherResult.name);
+        $('.temperatures').append(temp + ' °C');
+        var weathericonUrl =
+            '<img src= "http://openweathermap.org/img/wn/'
+            + weatherResult.weather[0].icon +
+            '@2x.png" alt="' + weatherResult.weather[0].description + '"/>'
+        $('.weather-ico').html(weathericonUrl);
+    });
+}
+
+function setAreaName(name) {
+    switch (name) {
+        case 'Seongnam-si':
+            $('.position').html('경기도 성남시')
+            break;
+        case 'Siheung-si':
+            $('.position').html('경기도 시흥시')
+            break;
+        default:
+            $('.position').html(name)
+            break;
+    }
+}
+
+getLocation();
