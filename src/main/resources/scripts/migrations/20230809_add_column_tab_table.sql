cd1d68849ceecdfdab04766a2c22952e08f03bb8
ChoiseU <csw980928@gmail.com>
2024-01-01 14:54:22 +0900

    feat : 데이터 베이스 및 초기 테이블 생성 sql 파일 추가

--- src/main/resources/scripts/migrations/20230809_add_column_tab_table.sql
+++
@@ -1,4 +0,0 @@
-use xe_management;
-
--- 탭 테이블 sort(정렬) 추가
-ALTER TABLE tb_tab ADD sort INT NOT NULL COMMENT '정렬순서' AFTER url;
--- src/main/resources/scripts/migrations/20230809_modify_column_tab_table.sql
+++
@@ -1,4 +0,0 @@
-use xe_management;
-
--- 탭 테이블 url 필수 제거
-ALTER TABLE tb_tab MODIFY url VARCHAR(64) NULL COMMENT '이동 URL';
