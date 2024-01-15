df9aa5f7bd9c4aab69758398568b2dfecb072103
choiseungwoo98 <csw980928@gmail.com>
2024-01-15 11:18:05 +0900

    feat : 음력 빈 구성

---
+++ src/main/java/com/choi/calender/domain/api/event/LunarBean.java
@@ -0,0 +1,51 @@
+package com.choi.calender.domain.api.event;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+@Getter
+@NoArgsConstructor
+public class LunarBean {
+    protected String no;
+    protected String lunYear;
+    protected String lunMonth;
+    protected String lunDay;
+    protected String solYear;
+    protected String solMonth;
+    protected String solDay;
+
+    public LunarBean(
+        String no,
+        String lunYear,
+        String lunMonth,
+        String lunDay,
+        String solYear,
+        String solMonth,
+        String solDay
+    ) {
+        this.no = no;
+        this.lunYear = lunYear;
+        this.lunMonth = lunMonth;
+        this.lunDay = lunDay;
+        this.solYear = solYear;
+        this.solMonth = solMonth;
+        this.solDay = solDay;
+    }
+
+    public LunarBean convertMapToBean(Map data, String no) {
+        return new LunarBean(
+            no,
+            String.valueOf(data.get("lunYear")),
+            String.valueOf(data.get("lunMonth")),
+            String.valueOf(data.get("lunDay")),
+            String.valueOf(data.get("solYear")),
+            String.valueOf(data.get("solMonth")),
+            String.valueOf(data.get("solDay"))
+        );
+    }
+}
